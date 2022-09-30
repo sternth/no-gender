e4a300453a94790904fc6e7369224a16870453c8
justdimaa <justdimaa.06asg@slmail.me>
2022-09-30 20:44:37 +0200

    Detect more articles
    einem/r Tutor/in -> einem Tutor

--- src/content/list.custom.js
+++ src/content/list.custom.js
@@ -8,7 +8,10 @@
     /**
      * Custom replace objects
      */
-    { regExp: /jede[:*_]r/, replace: 'jeder' },
-    { regExp: /ein[:*_]e/, replace: 'ein' },
+    { regExp: /jede[:*_\/]r/, replace: 'jeder' },
+    { regExp: /ein[:*_\/]e/, replace: 'ein' },
+    { regExp: /einem[:*_\/]r/, replace: 'einem' },
+    { regExp: /eines[:*_\/]einer/, replace: 'eines' },
+    { regExp: /den[:*_\/]die/, replace: 'den' },
   ]
 }(typeof window !== 'undefined' ? window : this))
